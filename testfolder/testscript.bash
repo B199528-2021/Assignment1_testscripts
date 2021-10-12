b2bb5d2c2c9fc4ea1518ff6b8432be7e1ad68f45
B199528-2021 <email_not_provided>
2021-10-13 00:17:20 +0100

    First bash script, ran fastqc, unzip file.

---
+++ testfolder/testscript.bash
@@ -0,0 +1,15 @@
+# add shebang line
+#!/bin/bash
+
+# create a variable for the fastq file to be analysed
+FQFILE="100k.C1-1-501_1"
+
+# run the software fastqc
+# save the output in the folder "fastqc_out_folder"
+fastqc -o fastqc_out_folder ~/data_assessment1/$FQFILE.fq.gz
+
+# go to the output folder
+cd fastqc_out_folder
+
+# open the zip file in the output folder
+unzip "*.zip"
