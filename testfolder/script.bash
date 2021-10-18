9b762ecd49f47cd059ec2f6519790c0a2778516b
B199528-2021 <email_not_provided>
2021-10-18 16:30:28 +0100

    analyse 6 reads only, as all reads would take too long, this is only for testing purpose, should be change at the end

--- testfolder/script.bash
+++ testfolder/script.bash
@@ -19,47 +19,35 @@ do
  ls $FQ > all_fastqc_files.txt
 done
 
-# remove first line
+# remove first line (that is the information about total number)
 sed -i "1d" all_fastqc_files.txt 
 
+# THIS IS ONLY FOR TESTING -> TODO: REMOVE AT THE END
+# pick only the first 6 reads
+sed -i -n "1,6 p" all_fastqc_files.txt
 
 # run fastqc for all fastq files
 while read CHECK
 do
+ 
  # save the output in the folder "fastqc_out_folder"
  fastqc -o fastqc_out_folder $DIRFQFILE/$CHECK
+ 
 done < all_fastqc_files.txt
+
+
+# unzip the zip file
+#cd fastqc_out_folder
+#unzip "*.zip"
+
 
 
 exit
 
 # test:
-# run fastqc for all fastq files
-for FQ in all_fastqc_files.txt
-do
- # save the output in the folder "fastqc_out_folder"
- fastqc -o fastqc_out_folder $DIRFQFILE/$FQ.fq.gz
-done
-
-
-exit
-
-
 
 # create a variable for the fastq file to be analysed
 FQFILE="100k.C1-1-501_1"
-
-
-
-# run the software fastqc
-# save the output in the folder "fastqc_out_folder"
-fastqc -o fastqc_out_folder $DIRFQFILE/$FQFILE.fq.gz
-
-# go to the output folder
-cd fastqc_out_folder
-
-# open the zip file in the output folder
-unzip "*.zip"
 
 # go to the unzipped folder
 cd ${FQFILE}_fastqc
