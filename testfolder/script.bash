4060d21f9f48424534b21c0a1182bbc08f34b745
B199528-2021 <email_not_provided>
2021-10-18 14:14:29 +0100

    put directory with input files into a variable

--- testfolder/script.bash
+++ testfolder/script.bash
@@ -1,11 +1,16 @@
 # add shebang line
 #!/bin/bash
 
-# create a variable for the fastq file to be analysed
-FQFILE="100k.C1-1-501_1"
+# directory with fastq files
+DIRFQFILE="/localdisk/data/BPSM/AY21/fastq"
 
 # create a folder for the output files
 mkdir fastqc_out_folder .
+
+
+
+# create a variable for the fastq file to be analysed
+FQFILE="100k.C1-1-501_1"
 
 # create a file for the fastqc summaries
 echo "This is an overview of all fastqc summary files." > summaries_fastqc.txt
@@ -13,7 +18,7 @@ echo "This is an overview of all fastqc summary files." > summaries_fastqc.txt
 
 # run the software fastqc
 # save the output in the folder "fastqc_out_folder"
-fastqc -o fastqc_out_folder ~/data_assessment1/$FQFILE.fq.gz
+fastqc -o fastqc_out_folder $DIRFQFILE/$FQFILE.fq.gz
 
 # go to the output folder
 cd fastqc_out_folder
@@ -27,7 +32,8 @@ cd ${FQFILE}_fastqc
 # save the summary into a file and tell the user
 cat summary.txt >> ../../summaries_fastqc.txt
 echo -e "\nPlease find the summary of fastqc quality check in the file 'summaries_fastqc'."
-echo "If the sequence failed the check, it might be a good idea to trim low quality reads and adapters."
+echo "You can also find the html files for each sequence in the folder 'fastqc_out_folder'."
+echo -e "\nIf the sequence failed the check, it might be a good idea to trim low quality reads and adapters."
 echo "You could run for example 'TrimGalore' to get better qualities of your sequences."
 echo "If you want to exclude sequences, please delete them from your folder and run this script again."
 
