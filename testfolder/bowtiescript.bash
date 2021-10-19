6145b761c99efad56b88c21f25ee80d07ac67585
B199528-2021 <email_not_provided>
2021-10-19 16:31:15 +0100

    copy genome sequence into a folder and unzip it

---
+++ testfolder/bowtiescript.bash
@@ -0,0 +1,22 @@
+# add shebang line
+#!/bin/bash
+
+# directory with reference genome sequence of Trypanosama congolense
+DIRTCONGO="/localdisk/data/BPSM/AY21/Tcongo_genome/"
+
+# directory with fastq files
+DIRFQFILE="/localdisk/data/BPSM/AY21/fastq"
+
+# delete folders and directories in case they alredy exist
+rm -r fastq_extracted_files
+rm -r reference_genome
+
+
+# copy genome sequence into a folder and unzip it to have a fasta file
+cp -r $DIRTCONGO reference_genome
+gunzip ./reference_genome/*.gz
+
+
+# find out directory
+echo "Here I am:"
+pwd
