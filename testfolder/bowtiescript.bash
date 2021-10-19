9f912e03d88e86e6ac2abf5cb9e5469765c92d10
B199528-2021 <email_not_provided>
2021-10-19 20:09:13 +0100

    use bowtie2

--- testfolder/bowtiescript.bash
+++ testfolder/bowtiescript.bash
@@ -24,27 +24,40 @@ DIRFQFILE="/localdisk/data/BPSM/AY21/fastq"
 #
 #
 ## extract fastq files from localdesk (DIRFQFILE="/localdisk/data/BPSM/AY21/fastq")
-#echo "Please wait. File being extracted..."
+#echo "Please wait, file being extracted..."
 #rm -r fastq_extracted_files
 #cp -r $DIRFQFILE fastq_extracted_files
 #gunzip ./fastq_extracted_files/*.fq.gz
 #
 #
+## pick read1 and read2 for running bowtie2
+#
+## delete the file endings ("_1" or "_2") in the file "all_fastqc_files.txt"
+#while read LINE
+#do
+# echo $LINE | rev | cut -c3- | rev >> tmpfile
+#done < all_fastqc_files.txt
+#mv tmpfile all_fastqc_files.txt 
+#
+#
+## delete the duplicates
+#uniq all_fastqc_files.txt > tmpfile
+#mv tmpfile all_fastqc_files.txt
+#
 #=============================================
 
-# pick read1 and read2 for running bowtie2
-
-# delete the file endings ("_1" or "_2") in the file "all_fastqc_files.txt"
-while read LINE
+rm -r bowtieoutput
+mkdir bowtieoutput
+echo "Please wait, bowtie2 is running ..."
+# go to reference_genome file
+cd ./reference_genome
+DIR="../fastq_extracted_files/"
+while read SEQ
 do
- echo $LINE | rev | cut -c3- | rev >> tmpfile
-done < all_fastqc_files.txt
-mv tmpfile all_fastqc_files.txt 
+ bowtie2 -x Tcongolense -1 ${DIR}/${SEQ}_1.fq -2 ${DIR}/${SEQ}_2.fq -S ../bowtieoutput/${SEQ}.sam
+done < ../all_fastqc_files.txt
 
 
-# delete the duplicates
-uniq all_fastqc_files.txt > tmpfile
-mv tmpfile all_fastqc_files.txt
 
 # find out directory
 echo "Here I am now:"
