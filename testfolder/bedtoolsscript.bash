fcbfb4141b6486a8972fcdaf6568b188d4988a40
B199528-2021 <email_not_provided>
2021-10-20 19:24:13 +0100

    generate bam files without newlines

--- testfolder/bedtoolsscript.bash
+++ testfolder/bedtoolsscript.bash
@@ -4,4 +4,21 @@
 # path of the bedfile
 BEDFILE="/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
+# path of the bam files
+BAMFILEPATH="./bowtieoutput"
 
+# put all the bam files into a variable
+while read SEQ
+do
+ echo "$SEQ.bam" >> tempfile
+ done < ./all_fastqc_files_unique.txt
+
+# delete newlines
+tr "\n" " " < tempfile > tempfile2
+rm tempfile
+mv tempfile2 bamfiles.txt
+
+
+
+
+echo "FINISHED"
