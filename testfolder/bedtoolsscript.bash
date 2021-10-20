903b020b8e058e8fdff70321f62ec60e13a7f2b3
B199528-2021 <email_not_provided>
2021-10-20 19:34:31 +0100

    add directory of bam files

--- testfolder/bedtoolsscript.bash
+++ testfolder/bedtoolsscript.bash
@@ -4,24 +4,26 @@
 # path of the bedfile
 BEDFILE="/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
-# path of the bam files
-BAMFILEPATH="./bowtieoutput"
-
 # put all the bam files into a variable
 while read SEQ
 do
- echo "$SEQ.bam" >> tempfile
+ echo "./bowtieoutput/$SEQ.bam" >> tempfile
  done < ./all_fastqc_files_unique.txt
 
 # delete newlines
 tr "\n" " " < tempfile > tempfile2
 rm tempfile
+rm bamfiles.txt
 mv tempfile2 bamfiles.txt
 
 # save it into a variable
 BAMFILES=$(<bamfiles.txt)
 
+
+
+
 echo $BAMFILES
+
 
 
 
