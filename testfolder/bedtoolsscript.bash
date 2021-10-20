0f34ed23746906cad23d33b60506713598abb3be
B199528-2021 <email_not_provided>
2021-10-20 18:15:03 +0100

    deleted converting bam to bed formats

--- testfolder/bedtoolsscript.bash
+++ testfolder/bedtoolsscript.bash
@@ -4,9 +4,4 @@
 # path of the bedfile
 BEDFILE="/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
-# convert bam alignments to BED formats
-for x in ./bowtieoutput/*.bam
-do
- bedtools bamtobed -i "$x" > "${x%.bam}.bed"
-done
-echo "finished"
+
