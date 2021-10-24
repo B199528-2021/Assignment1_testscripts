1fa0beb1bf2ee188e0158fa81d5968d216d28c96
B199528-2021 <email_not_provided>
2021-10-25 00:07:45 +0100

    average calculated for all groups

--- testfolder/analysisscript.bash
+++ testfolder/analysisscript.bash
@@ -4,8 +4,40 @@
 echo "Calculating the average now ..."
 
 
+while read ONEGROUP
+do
+
+ cp ./bedtoolsoutput/bedtoolsoutput.$ONEGROUP.tsv ./bedtoolsoutput/averagecounts.$ONEGROUP.tsv
+
+ # delete the first columns (gene + description)
+ cut -f -2 ./bedtoolsoutput/averagecounts.$ONEGROUP.tsv > tempfile
+
+ # calculate average
+ awk '{s=0; for (i=1;i<=NF;i++)s+=$i; print s/NF;}' tempfile > ./bedtoolsoutput/averagecounts.$ONEGROUP.tsv
+ rm tempfile
+
+ # join the columns
+ cut -f -2 ./bedtoolsoutput/bedtoolsoutput.$ONEGROUP.tsv | paste ./bedtoolsoutput/averagecounts.$ONEGROUP.tsv - > temp
+ mv temp ./bedtoolsoutput/average.$ONEGROUP.tsv
+
+ rm ./bedtoolsoutput/averagecounts.$ONEGROUP.tsv
+
+ echo "Average for $ONEGROUP calculated."
+
+done < ./groups.txt
+
+echo "Please find the average results in the folder 'bedtoolsoutput'."
+
+echo "This is the current directory:"
+pwd
+
+exit
+
+
 # go into the bedtoolsoutput folder
 cd ./bedtoolsoutput
+
+
 
 
 #-------------
