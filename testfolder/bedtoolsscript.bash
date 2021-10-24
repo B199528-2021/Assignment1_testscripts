a273e308694df7f5d1f1299cb7ffe3219e616754
B199528-2021 <email_not_provided>
2021-10-24 10:56:23 +0100

    update bedtoolsscript.bash

--- testfolder/bedtoolsscript.bash
+++ testfolder/bedtoolsscript.bash
@@ -4,6 +4,8 @@
 # path of the bedfile
 BEDFILE="/localdisk/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
+echo "Please wait, bedtools is running ..."
+
 # put all the bam files into a variable
 while read SEQ
 do
@@ -19,10 +21,16 @@ mv tempfile2 bamfiles.txt
 # save it into a variable
 BAMFILES=$(<bamfiles.txt)
 
+# run bedtolls for all bam alignments
+bedtools multicov -bams $BAMFILES -bed $BEDFILE > bedtoolsoutput.txt
+
+# delete bamfiles.txt
+rm bamfiles.txt
+
+echo "Bedtools finished."
 
 
 
-echo $BAMFILES
 
 
 
