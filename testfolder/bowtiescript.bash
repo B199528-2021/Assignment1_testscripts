7752cfe55ff2ced8df4ef6aebde26b8a0643e17d
B199528-2021 <email_not_provided>
2021-10-24 20:51:08 +0100

    bowtiescript.bash with all sequences

--- testfolder/bowtiescript.bash
+++ testfolder/bowtiescript.bash
@@ -7,30 +7,27 @@ DIRTCONGO="/localdisk/data/BPSM/AY21/Tcongo_genome/"
 # directory with fastq files
 DIRFQFILE="/localdisk/data/BPSM/AY21/fastq"
 
-#=============================================
-# TODO: activate (uncomment) at the end!!!
-## copy genome sequence into a folder and unzip it to have a fasta file
-#rm -r reference_genome
-#cp -r $DIRTCONGO reference_genome
-#gunzip ./reference_genome/*.gz
-#
-## create an index for the reference genome
-## references: http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml#obtaining-bowtie-2
-## references: https://www.youtube.com/watch?v=fSnAeYHnPCw
-#cd ./reference_genome
-#bowtie2-build ./*fasta Tcongolense
-#
-#cd ..
-#
-#
-## extract fastq files from localdesk (DIRFQFILE="/localdisk/data/BPSM/AY21/fastq")
-#echo "Please wait, file being extracted..."
-#rm -r fastq_extracted_files
-#cp -r $DIRFQFILE fastq_extracted_files
-#gunzip ./fastq_extracted_files/*.fq.gz
-#
-#
-#=============================================
+# copy genome sequence into a folder and unzip it to have a fasta file
+rm -r reference_genome
+cp -r $DIRTCONGO reference_genome
+gunzip ./reference_genome/*.gz
+
+# create an index for the reference genome
+# references: http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml#obtaining-bowtie-2
+# references: https://www.youtube.com/watch?v=fSnAeYHnPCw
+cd ./reference_genome
+bowtie2-build ./*fasta Tcongolense
+
+cd ..
+
+
+# extract fastq files from localdesk (DIRFQFILE="/localdisk/data/BPSM/AY21/fastq")
+echo "Please wait, file being extracted..."
+rm -r fastq_extracted_files
+cp -r $DIRFQFILE fastq_extracted_files
+gunzip ./fastq_extracted_files/*.fq.gz
+
+#-------------
 
 # pick read1 and read2 for running bowtie2
 
@@ -72,5 +69,6 @@ done < ./all_fastqc_files_unique.txt
 
 
 # find out directory
-echo "Here I am now:"
+echo "Thanks for waiting, bowtie2 has finished."
+echo "This is current directory:"
 pwd
